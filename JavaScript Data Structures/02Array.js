f00100c49177b501d2958f6f95c7b1c57534c2cf
arunkdorai <arunkumar.k0786@gmail.com>
2024-08-28 21:04:47 +0530

    JavaScript Data Structures till V-16 Linked List Print

---
+++ JavaScript Data Structures/02Array.js
@@ -0,0 +1,20 @@
+const arr = [1, 2, 3, 'Vishwas']
+arr.push(4)
+
+for(const item of arr) {
+    console.log(item)
+}
+
+arr.unshift(0)
+
+console.log(arr)
+
+arr.pop()
+
+console.log(arr)
+
+arr.shift()
+
+console.log(arr)
+
+// learn about map, filter, reduce, concat, slice and splice
