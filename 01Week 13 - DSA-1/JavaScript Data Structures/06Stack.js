f7e97a1ad8a6d46f6000884b2aa9e91a05f10eeb
arunkdorai <arunkumar.k0786@gmail.com>
2024-09-21 17:58:15 +0530

    till 08

--- 01Week 13 - DSA-1/JavaScript Data Structures/06Stack.js
+++ 01Week 13 - DSA-1/JavaScript Data Structures/06Stack.js
@@ -1,4 +1,25 @@
+/* Stack
 
+The stack data structure is a sequential collection of elements that follows the principle of Last In First Out(LIFO).
+
+This means, the last element inserted into the stack is first element to be removed.
+
+Ex: A stack of plates. The last plate placed on top of the stack is also the first plate removed from the stack.
+
+Stack is an abstract data type. It is defined by its behavior rather than being a mathematical model.
+
+The stack data structure supports two main operations
+Push, which adds an element to the collection
+Pop, which removes the most recently added element from the collection.
+
+Stack Usage
+-Browser history tracking
+-Undo operation when typing
+-Expression conversions
+-Call stack in JavaScript runtime
+*/
+
+// Stack Using Array
 
 class Stack {
     constructor() {
@@ -63,3 +84,43 @@ stack.print(); // Output: 1,2
 
 // Check if the stack is empty
 console.log("Is the stack empty?", stack.isEmpty());
+
+
+/* Stack Using Objects
+
+class Stack {
+  constructor() {
+    this.items = {};
+    this.head = 0;
+  }
+
+  push(element) {
+    this.items[this.head] = element;
+    this.head++;
+  }
+
+  pop() {
+    const item = this.items[this.head - 1];
+    delete this.items[this.head - 1];
+    this.head--;
+    return item;
+  }
+
+  peek() {
+    return this.items[this.head - 1];
+  }
+
+  size() {
+    return this.head;
+  }
+
+  isEmpty() {
+    return this.head === 0;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+*/
--- 01Week 13 - DSA-1/JavaScript Data Structures/08Queue.js
+++ 01Week 13 - DSA-1/JavaScript Data Structures/08Queue.js
@@ -1,3 +1,24 @@
+/* Queue
+
+The queue data structure is a sequential collection of elements that follows the principle of First In First Out (FIFO).
+
+The first element inserted into the queue is first element to be removed.
+
+A queue of people. People enter the queue at one end (rear/tail) and leave the queue from the other end (front/head).
+
+Queue is an abstract data type. It is defined by its behavior rather then being a mathematical model.
+
+The Queue data structure supports two main operations 
+-Enqueue, which adds an element to the rear/tail of the collection.
+-Dequeue, which removes an element from the front/head of the collection.
+
+Queue Usage
+-Printers, when we try to print multiple documents
+-CPU task scheduling 
+-Callback queue in JavaScript runtime to determine the order in which our code executes.
+*/
+
+// Queue Using Arrays
 
 class Queue {
     constructor() {
@@ -17,7 +38,7 @@ class Queue {
         return this.items.shift();
     }
 
-    // Peek: Get the front element of the queue
+    // Peek: Get the front element of the queue without removing it
     peek() {
         if (this.isEmpty()) {
             return "Queue is empty";
@@ -50,3 +71,58 @@ queue.print(); // Output: 1,2,3
 console.log(queue.dequeue()); // Output: 1
 queue.print(); // Output: 2,3
 
+
+/* V10 - Queue Implementation optimised
+
+Queue Using Objects
+
+class Queue {
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const queue = new Queue();
+console.log(queue.isEmpty());
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log(queue.size());
+queue.print();
+console.log(queue.dequeue());
+console.log(queue.peek());
+console.log(queue.isEmpty());
+queue.print();
+
+
+*/
