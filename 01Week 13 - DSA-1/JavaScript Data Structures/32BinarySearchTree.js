20599c3793ff52ad9d0edff13d7986a82bd9e09b
arunkdorai <arunkumar.k0786@gmail.com>
2024-09-28 19:23:33 +0530

    Till 33BSTClass

---
+++ 01Week 13 - DSA-1/JavaScript Data Structures/32BinarySearchTree.js
@@ -0,0 +1,21 @@
+/* Binary Search Tree Overview
+
+Binary Tree: A binary tree is a tree data structure in which each node 
+has at most two children. They are referred to as left child and right child.
+
+Binary Search Tree (BST): A BST is a binary tree that has the following two properties:
+-The value of each left node must be smaller than the parent node.
+-The value of each right node must be greater than the parent node.
+-The above two is in addition to each node having at most two children.
+
+Binary Search Tree Operations:
+-Insertion - To add a node to the tree.
+-Search - To find a node given its value.
+-DFS (Depth First Search) & BFS (Breadth First Search) - To visit all nodes in the tree.
+-Deletion - To remove a node given its value.
+
+Binary Search Tree Usage:
+-Searching
+-Sorting
+-To implement abstract data types such as lookup tables and priority queues.
+*/
